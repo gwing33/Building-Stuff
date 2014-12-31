96f767daf04ca7410dfdf8150f7732b1a29be6bf
Michael Bruner <brunerm6@gmail.com>
2014-12-31 12:18:35 -0600

    Initial Solution Files

---
+++ Building-Stuff/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Building_Stuff.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.title = "Building Stuff";
+            return View();
+        }
+    }
+}
